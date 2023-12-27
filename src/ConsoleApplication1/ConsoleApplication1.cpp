c0a2bfe9b02050e450723dbc71c7ca6ac596710c
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2023-12-27 19:05:43 +0800

    put package back

---
+++ src/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,137 @@
+﻿#include <sqlite3.h>
+#include <iostream>
+
+using namespace std;
+
+bool found = false; // Flag to check if data is found
+
+static int selectData(const char* s, const char* UserName, const char* Password);
+static int callback(void* NotUsed, int argc, char** argv, char** azColName);
+static int insertData(const char* s, const char* username, const char* password, const char* fullname, const char* emaiorphone);
+
+int main() {
+    const char* dir = "C:\\Users\\ADMIN\\Desktop\\database\\data.db";
+
+    char UserName[50];
+    char Password[50];
+    char FullName[50];
+    char EmailOrPhone[50];
+
+    int choose;
+    cout << "Sign up (1) OR Sign in (2): ";
+    cin >> choose;
+
+    if (choose == 1) {
+        cout << "Name: ";
+        cin >> UserName;
+
+        cout << "Password: ";
+        cin >> Password;
+
+        cout << "FullName: ";
+        cin >> FullName;
+
+        cout << "Email Or Phone: ";
+        cin >> EmailOrPhone;
+
+        insertData(dir, UserName, Password, FullName, EmailOrPhone);
+    }
+    else if (choose == 2) {
+        cout << "Name: ";
+        cin >> UserName;
+
+        cout << "Password: ";
+        cin >> Password;
+
+        selectData(dir, UserName, Password);
+    }
+    else {
+        cout << "Invalid choice." << endl;
+    }
+
+    return 0;
+}
+
+static int selectData(const char* s, const char* UserName, const char* Password) {
+    sqlite3* DB;
+    char* messageError;
+
+    string sql_username = "SELECT * FROM Player WHERE Username = '" + string(UserName) + "'";
+    string sql = "SELECT * FROM Player WHERE Username = '" + string(UserName) + "' AND Password = '" + string(Password) + "'";
+
+    int exit = sqlite3_open(s, &DB);
+
+    if (exit != SQLITE_OK) {
+        cerr << "Error opening the database." << endl;
+        return 1;
+    }
+
+    exit = sqlite3_exec(DB, sql_username.c_str(), callback, NULL, &messageError);
+
+    if (exit != SQLITE_OK) {
+        cerr << "Error in selectData function." << endl;
+        sqlite3_free(messageError);
+    }
+    else {
+        if (found == true) {
+            found = false;
+            exit = sqlite3_exec(DB, sql.c_str(), callback, NULL, &messageError);
+            if (exit != SQLITE_OK) {
+                cerr << "Error in selectData function." << endl;
+                sqlite3_free(messageError);
+            }
+            else {
+                if (found == true) {
+                    printf("Login successfully!\n");
+                }
+                else {
+                    printf("Incorrect password!\n");
+                }
+            }
+        }
+        else {
+            cout << "User not found!" << endl;
+        }
+    }
+
+    sqlite3_close(DB); // Close the connection after use
+
+    return 0;
+}
+
+static int insertData(const char* s, const char* username, const char* password, const char* fullname, const char* emaiorphone)
+{
+    sqlite3* DB;
+    char* messageError;
+
+    string sql = "INSERT INTO Player(Username, Password, Fullname, EmailOrPhone) VALUES('" + string(username) + "', '" + string(password) + "', '" + string(fullname) + "', '" + emaiorphone + "')";
+
+    int exit = sqlite3_open(s, &DB);
+    /* An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here */
+    exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+
+    if (exit != SQLITE_OK) {
+        cerr << "Error in insertData function." << endl;
+        sqlite3_free(messageError);
+    }
+    else {
+        cout << "Records inserted successfully!" << endl;
+    }
+
+    sqlite3_close(DB); // Close the connection after use
+
+    return 0;
+}
+
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    found = true; // Set the flag when data is found
+
+    for (int i = 0; i < argc; i++) {
+        // column name and value
+        cout << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
