e9cdd99c002d92037a8934a50f4933542d8cb415
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-01-04 16:14:11 +0800

    small edit

--- build_release.sh
+++ build_release.sh
@@ -1,1 +1,1 @@
-set -x ; cd ui && npm run build && cd .. ; cd build ; cmake -S .. -B release/ -DCMAKE_BUILD_TYPE=Release && cmake --build release/
+set -x ; cd ui && npm run build && cd .. ; cd build ; rm release/src/html_const.cpp ; cmake -S .. -B release/ -DCMAKE_BUILD_TYPE=Release && cmake --build release/
