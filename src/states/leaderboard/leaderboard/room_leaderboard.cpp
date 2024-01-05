24c4fe3e2e99ccd53ecb49d8ecc2c2f4768570e8
lehangan <lehangan30@gmail.com>
2024-01-05 10:17:13 +0700

    fix: leader_board state

--- src/states/leaderboard/leaderboard/room_leaderboard.cpp
+++ src/states/leaderboard/leaderboard/room_leaderboard.cpp
@@ -11,60 +11,63 @@
 
 using namespace std;
 
-// bool comparePlayers( pair<int, Player>& a, pair<int, Player>& b) {
-//     return a.second.currentScore > b.second.currentScore;
-// }
+vector<pair<int, Player>> sortPlayers(const unordered_map<int, Player>& players)
+{
+    // Copy elements to a vector
+    vector<pair<int, Player>> sortedPlayers(players.begin(), players.end());
 
-// unordered_map<int, Player> getSortedPlayerMap(unordered_map<int, Player>& inputMap) {
-//     // Convert input map to a vector of pairs for sorting
-//     vector<pair<int, Player>> playerVector(inputMap.begin(), inputMap.end());
+    // Sort the vector by PlayerID
+    sort(sortedPlayers.begin(), sortedPlayers.end(),
+     [](const auto& lhs, const auto& rhs) {
+        return lhs.second.currentScore < rhs.second.currentScore;
+    });
 
-//     // Sort the vector using the custom comparator
-//     sort(playerVector.begin(), playerVector.end(), comparePlayers);
+    return sortedPlayers;
+}
 
-//     return inputMap;
-// }
 
-// Player getPlayerWin(unordered_map<int, Player>& inputMap){
+Player getPlayerWin(unordered_map<int, Player>& inputMap){
 
-// 	unordered_map<int, Player> map_sort = getSortedPlayerMap(inputMap);
-// 	auto first_one = map_sort.begin();
-// 	return first_one->second;
-// }
+	vector<pair<int, Player>> sortedVec = sortPlayers(players);
+
+    pair<int, Player> p_win = sortedVec.back();
+
+	return p_win.second;
+}
 
 // // for saving player (account) statistic point
-// int getPlayerPoint(unordered_map<int, Player>& inputMap, int playerID){
-// 	for (auto& pair : inputMap) {
-//         if( pair.first == playerID){
-// 			Player& player = pair.second;
-// 			return player.currentScore;
-// 		}
-// 		else{
-// 			// no find this playerID
-// 		}
-// 	}
-// 	return 0;
-// }
+int getPlayerPoint(unordered_map<int, Player>& inputMap, int playerID){
+	for (auto& pair : inputMap) {
+        if( pair.first == playerID){
+			Player& player = pair.second;
+			return player.currentScore;
+		}
+		else{
+			// no find this playerID
+		}
+	}
+	return 0;
+}
 
-// int plusPlayerPoint(unordered_map<int, Player>& inputMap, int playerID, int plusPoint){
-// 	for (auto& pair : inputMap) {
-//         if( pair.first == playerID){
-// 			Player& player = pair.second;
-// 			player.currentScore += plusPoint;
-// 			return plusPoint;
-// 		}
-// 		else{
-// 			// no find this playerID
-// 		}
-// 	}
-// 	return 0;
-// }
+int plusPlayerPoint(unordered_map<int, Player>& inputMap, int playerID, int plusPoint){
+	for (auto& pair : inputMap) {
+        if( pair.first == playerID){
+			Player& player = pair.second;
+			player.currentScore += plusPoint;
+			return plusPoint;
+		}
+		else{
+			// no find this playerID
+		}
+	}
+	return 0;
+}
 
 void LeaderboardState::handle(const BaseMsg& msg, int playerID) {
 	DEBUG_PRINT("  (LeaderboardState) " + msg.toString());
 
-	//unordered_map<int,Player> sort_playerMap;
-	// sort_playerMap = room->playerMap;
+	unordered_map<int,Player> sort_playerMap;
+	sort_playerMap = room->playerMap;
 
 	switch (msg.type())
 	{
