4f0a9a41d758678d899df28045ec164682a40354
lehangan <lehangan30@gmail.com>
2024-01-11 18:45:01 +0700

    Revert "Merge branch 'main' into ngan"
    This reverts commit c34fe2c3499c8017a7c7b23470585235f48881d8, reversing
    changes made to b22a033859829cee8b87cb7f1723c9293487bf76.

--- ui/src/room/MeTest.svelte
+++ ui/src/room/MeTest.svelte
@@ -1,111 +1,93 @@
 <script>
-	import { Canvas } from 'svelte-canvas';
-	import Balls from './Balls.svelte';
+  import { Canvas } from 'svelte-canvas';
+  import Balls from './Balls.svelte';
 
-	import Palette from './detached/Palette.svelte'
-	import { drawSettings } from '../store/store';
+  let balls = [
+    { color: 'tomato', x: 0.5, y: 0.333 },
+    { color: 'goldenrod', x: 0.333, y: 0.625 },
+    { color: 'mediumturquoise', x: 0.667, y: 0.625 },
+  ];
 
-	let balls = [
-		{ color: 'tomato', x: 0.5, y: 0.333 },
-		{ color: 'goldenrod', x: 0.333, y: 0.625 },
-		{ color: 'mediumturquoise', x: 0.667, y: 0.625 },
-	];
-
-	const reorder = (color) => {
-		balls = balls
-			.filter((c) => c.color !== color)
-			.concat(balls.filter((c) => c.color === color));
-	};
+  const reorder = (color) => {
+    balls = balls
+      .filter((c) => c.color !== color)
+      .concat(balls.filter((c) => c.color === color));
+  };
 
 
-	const setUp = (canvas) => {
-		const context = canvas?.getContext();
+  const setUp = (context) => {
+    if (canvas) {
+      context.strokeStyle = '#000'
+    }
 
-		if (context) {
-			context.strokeStyle = brushcolor;
-			context.lineWidth = brushsize;
+    console.log("Triggerd setUp");
+    return canvas?.getContext();
+  }
+  // Drawing code
+  let canvas;
+  $: context = canvas?.getContext();
 
-			console.log("Triggeredd setUp");
-		} else {
-			console.log("setUp Failed");
-		}
+  $: if(context) {
 
-		return context;
-	}
+    context.strokeStyle = '#000'  
+  }
 
-	let brushcolor = '#000';
-	let brushsize = 5;
+  let start
+  let t, l;
+  let isDrawing = false;
 
-	const changeBrush = (color, size) => {
-		if(context) {
-			console.log("Change Brush success");
-			context.strokeStyle = brushcolor;
-			context.lineWidth = brushsize;
-		} else {
-			console.log("changeBrush failed")
-		}
-	}
-
-	// Drawing code
-	let canvas;
-	$: context = setUp(canvas);
-	$: changeBrush(brushcolor, brushsize);
-
-	let start
-	let t, l;
-	let isDrawing = false;
-
-	const clearCanvas = () => { 
-		const { width, height } = canvas.getCanvas();
-		context.clearRect(0, 0, width, height); 
-		console.log("FUCK");
-	}
-
-	const handleStart = ({ offsetX: x, offsetY: y }) => { 
-		// if(color === background) {
-		//   
-		// } else {
-			isDrawing = true
-			start = { x, y }
-		// }
-	};
-	
-	const handleEnd = () => { isDrawing = false };
-	const handleMove = ({ offsetX: x1, offsetY: y1 }) => {
-		if(!isDrawing) return;
-		
-		const { x, y } = start;
-			context.lineCap = 'round';
-		context.beginPath();
-		context.moveTo(x, y);
-		context.lineTo(x1, y1);
-		context.stroke();
-		
-		start = { x: x1, y: y1 };
-	};
+  const handleStart = ({ offsetX: x, offsetY: y }) => { 
+    // if(color === background) {
+    //   context.clearRect(0, 0, width, height)
+    // } else {
+      isDrawing = true
+      start = { x, y }
+    // }
+  };
+  
+  const handleEnd = () => { isDrawing = false };
+  const handleMove = ({ offsetX: x1, offsetY: y1 }) => {
+    if(!isDrawing) return;
+    
+    const { x, y } = start;
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x1, y1);
+    context.stroke();
+    
+    start = { x: x1, y: y1 };
+  };
 </script>
 
-<Palette bind:brushcolor bind:brushsize {clearCanvas}/>
-
 <Canvas bind:this={canvas}
-	on:mousedown={handleStart}  
-	on:mouseup={handleEnd}
-	on:mouseleave={handleEnd}
-	on:mousemove={handleMove}
+  on:mousedown={handleStart}  
+  on:mouseup={handleEnd}
+  on:mouseleave={handleEnd}
+  on:mousemove={handleMove}
 
-	on:touchstart={e => {
-		const { clientX, clientY } = e.touches[0]
-		handleStart({
-			offsetX: clientX - l,
-			offsetY: clientY - t
-		})
-	}}
-	on:touchmove={e => {
-		const { clientX, clientY } = e.touches[0]
-		handleMove({
-			offsetX: clientX - l,
-			offsetY: clientY - t
-		})
-	}}
-	on:touchend={handleEnd}
-/>
+  on:touchstart={e => {
+    const { clientX, clientY } = e.touches[0]
+    handleStart({
+      offsetX: clientX - l,
+      offsetY: clientY - t
+    })
+  }}
+  on:touchmove={e => {
+    const { clientX, clientY } = e.touches[0]
+    handleMove({
+      offsetX: clientX - l,
+      offsetY: clientY - t
+    })
+  }}
+  on:touchend={handleEnd}
+>
+  {#each balls as { color, x, y } (color)}
+    <Balls
+      {color}
+      {x}
+      {y}
+      on:mousedown={() => reorder(color)}
+      on:touchstart={() => reorder(color)}
+    />
+  {/each}
+</Canvas>
